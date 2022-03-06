3e01f9fab90d4b1f611a6654597b38025a818146
Willian Rafael <willia.rafael18@gmail.com>
2022-03-06 14:33:42 -0300

    criando formulario

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="materialize/css/materialize.min.css"> 
+        <title>Login</title>
+      
+    </head>
+    <body>
+       
+        <nav class="blue-grey" >
+            <div class="nav-wrapper container">
+                <div class="brand-logo light">Sistema de Cadastro</div>
+                <ul class="right">
+                    <li><a href=""><i class="material-icons left"></i>Cadastro</a></li>
+                    <li><a href=""><i class="material-icons left"></i>Consultas</a></li>
+                </ul>
+
+            </div>
+        </nav>
+        
+        <!--ARQUIVOS JQUERY E JAVASCRIPT-->
+        <script type="text/javascript" src="bootstrap-5.1.3/js/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="bootstrap-5.1.3/js/index.umd.js"></script>
+        
+        <!--INICIALIZÇÃO JQUERY-->
+        <script type="text/javascript">
+           $(document).ready(function (){
+               
+           })
+        </script>
+    </body>
+</html>
